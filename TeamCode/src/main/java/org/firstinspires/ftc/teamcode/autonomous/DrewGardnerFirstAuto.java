5c39f5ce4fa10c9a69843076ea2cccdffd03a394
gardnera27267 <gardnera27267@cghsnc.org>
2023-09-16 10:58:52 -0400

    DrewGardnerFirstAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DrewGardnerFirstAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+@Autonomous(name = "Drew Gardner First Auto", group = "Outreach")
+public class DrewGardnerFirstAuto extends AbstractAutonomous{
+
+    /** @noinspection UnresolvedClassReferenceRepair*/
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(this.hardwareMap);
+        waitForStart();
+    }
+}
